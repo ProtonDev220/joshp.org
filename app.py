00e19f9acf04e1a3169f5d6b52266ceb6eced1a6
Proton15 <113076857+Proton15@users.noreply.github.com>
2023-04-09 20:03:17 -0600

    server properties addition.gitignore added

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 from flask import Flask, render_template
+import configparser
 
+# read flask server properties file
+config = configparser.ConfigParser()
+config.read('server.properties')
+        
+# init flask
 app = Flask(__name__)
 
+# index route
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# signin page route
 @app.route('/signin')
 def signin():
     return render_template('signin.html')
 
+# read server port from flask properties file
+serverPort = config.get('server.general', 'serverPort')
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=serverPort)
